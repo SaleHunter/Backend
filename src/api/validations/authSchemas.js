54326ab555b0f4a979241ad43b85bcd208cf507d
ahmedsamir-dev <ahmedsamirwarda22@gmail.com>
2022-02-13 19:10:27 +0200

    updated sign up vaildation

--- src/api/validations/authSchemas.js
+++ src/api/validations/authSchemas.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const schemas = {
   signup: Joi.object()
     .keys({
-      fullname: Joi.string().alphanum().min(3).max(30).required(),
+      fullname: Joi.string().min(3).max(30).required(),
 
       password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
 
