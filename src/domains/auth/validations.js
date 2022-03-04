73c424fb5920aff436e8a806301ce5f11ae7f59d
YoussefMahmod <youss3fmahmoud@gmail.com>
2022-03-05 01:45:13 +0200

    adding signup Joi validations.

--- src/domains/auth/validations.js
+++ src/domains/auth/validations.js
@@ -46,6 +46,42 @@ class Validation {
 
     return schema;
   }
+  async signup(req, res, next) {
+    const schema = Joi.object()
+      .keys({
+        email: Joi.string().email().required().messages({
+          'string.base': 'Email is must be a string',
+          'any.required': 'Email is required',
+          'string.email': 'Please provide a valid email address',
+        }),
+
+        password: Joi.string()
+          .min(8)
+          .pattern(new RegExp(/^[ A-Za-z0-9_@./#&+-]*$/)) // Accepts only letters, numbers and '-@./#&'
+          .required()
+          .messages({
+            'string.base': 'Password is must be string',
+            'string.min': 'The minimum password length is 8',
+            'string.pattern.base': 'Please enter a valid password',
+            'any.required': 'Password is required',
+          }),
+        passwordConfirmation: Joi.ref('password'),
+        fullname: Joi.string().min(3).max(30).required().messages({
+          'string.base': 'Username must be string',
+          'string.min': 'The minimum Username length is 3',
+          'string.max': 'The maximum Username length is 30',
+          'any.required': 'Username is required',
+        }),
+        profile_img: Joi.string().messages({
+          'string.base': 'Invalid Image.',
+        }),
+        access_token: [Joi.string(), Joi.number()],
+      })
+      .xor('password', 'access_token')
+      .with('password', 'passwordConfirmation');
+    schema.validateAsync(req.body);
+    next();
+  }
 }
 
 module.exports = new Validation();
