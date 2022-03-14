dbd8b6b813908165bef39865c85de1a15f0ad5db
ahmedsamir-dev <ahmedsamirwarda22@gmail.com>
2022-03-14 11:34:00 +0200

    Finsished the user domain

--- src/domains/user/controllers.js
+++ src/domains/user/controllers.js
@@ -28,7 +28,7 @@ class Controller {
 
     // //Set the jwt cookie
     // helper.setJWTCookie(jwToken, res);
-    res.cookie('JWT', jwt, {
+    res.cookie('JWT', jwToken, {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), // expires in 90 days
     });
 
