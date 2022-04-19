5ffcde615335eaab8a65af19b19e0338655adb5c
ahmedsamir-dev <ahmedsamirwarda22@gmail.com>
2022-04-19 14:17:20 +0200

    Fixing CORS Policy

--- src/app.js
+++ src/app.js
@@ -86,7 +86,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-
 //Request Logger
 app.use(logger('dev'));
 
@@ -98,7 +97,6 @@ app.use(cookieParser());
 
 //Configure Swagger API documentation
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-
 
 // All Routes
 //Product Router
