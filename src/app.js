f0adbdafeab6190eef4112e00639c790a5cf9f3c
ahmedsamir-dev <ahmedsamirwarda22@gmail.com>
2022-04-20 23:48:59 +0200

    Trying to fix CORS

--- src/app.js
+++ src/app.js
@@ -92,7 +92,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Expose-Headers', '*, Authorization');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    '*, Origin, X-Requested-With, Content-Type, Accept, Authorization, language, lan, lat'
   );
   res.header(
     'Access-Control-Allow-Methods',
