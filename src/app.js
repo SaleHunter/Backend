46925d2402ccb52f642282eb926ef24ff5688694
ahmedsamir-dev <ahmedsamirwarda22@gmail.com>
2022-05-08 20:07:25 +0200

    test2

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.use(function (req, res, next) {
   res.set('Access-Control-Expose-Headers', '*, Authorization');
   res.set(
     'Access-Control-Allow-Headers',
-    '*, Origin, X-Requested-With, Content-Type, Accept, Authorization, language, lan, lat'
+    '*, Origin, X-Requested-With, Content-Type, Accept, Authorization, language, lon, lat'
   );
   res.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS,PATCH');
 
